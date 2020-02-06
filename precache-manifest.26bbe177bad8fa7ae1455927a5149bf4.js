be68ab172714d3e82b0a72e1bdcf5159d409050d
PratikshaMore <morepratiksha9@gmail.com>
2020-02-05 21:53:44 -0500

    Updates

---
+++ precache-manifest.26bbe177bad8fa7ae1455927a5149bf4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "db166f45722003d534c48d32aa8952c3",
+    "url": "/portfolio/static/media/pizza.db166f45.jpg"
+  },
+  {
+    "revision": "d8acf6466ee93e73d02fad19863f96e7",
+    "url": "/portfolio/static/media/Resume_PratikshaMore.d8acf646.pdf"
+  },
+  {
+    "revision": "0fad4c0f10d57b530fe4",
+    "url": "/portfolio/static/js/runtime~main.0fad4c0f.js"
+  },
+  {
+    "revision": "eda177a106aa08cc3dd1",
+    "url": "/portfolio/static/js/main.eda177a1.chunk.js"
+  },
+  {
+    "revision": "646f3d2a74bef00b3a8f",
+    "url": "/portfolio/static/js/2.646f3d2a.chunk.js"
+  },
+  {
+    "revision": "eda177a106aa08cc3dd1",
+    "url": "/portfolio/static/css/main.bb6443df.chunk.css"
+  },
+  {
+    "revision": "5de152220f89341a65d79f24681f2f8b",
+    "url": "/portfolio/index.html"
+  }
+];
